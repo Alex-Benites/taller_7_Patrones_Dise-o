e905fff9c442ebe700c5dc41ffed27f1f62be86f
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2023-12-06 22:01:52 -0500

    Craeacion de Codigo del patron de diseño Chain of Responsibility del parrafo 3

---
+++ src/Parrafo2/ViaAerea.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parrafo2;
+
+/**
+ *
+ * @author Abeni
+ */
+public class ViaAerea implements Transporte{
+
+    private String Id;
+
+    @Override
+    public void Transportar() {
+        // TODO - implement Avion.Transportar
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void SetDestino() {
+        // TODO - implement Avion.SetDestino
+        throw new UnsupportedOperationException();
+    }
+
+}
